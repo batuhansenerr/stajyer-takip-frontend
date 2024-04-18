6220d9cb432012e28d0b5f75aa7521e1ae95b5a8
abatuhan06 <abatuhan0666@gmail.com>
2024-04-18 12:20:13 +0300

    create comment component added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from './Home'
 import Profile from './pages/Profile/Profile';
 import Project from './pages/Project/Project';
+import PrivateRoutes from './utils/PrivateRoutes';
+import CreateComment from './component/Comment/CreateComment';
 
 function App() {
   
@@ -17,6 +19,7 @@ function App() {
           <Route exact path="/register" element={<SignUp/>} />
           <Route exact path="/login" element={<SignIn/>} />
           <Route exact path="/profile" element={<Profile/>} />
+          <Route exact path="/project/:id/comments/new" element={<CreateComment/>} />
           <Route exact path="/project/:id" element={<Project/>} />
         </Routes>
       </Router>
---
+++ src/component/Comment/CreateComment.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const CreateComment = () => {
+
+    const {id} = useParams()
+    const navigate = useNavigate()
+
+    const [newcomment, setComment] = useState({
+        comment: "",
+        project_id: id
+    })
+
+    const { comment } = newcomment
+
+    const createComment = async(e)=>{
+        e.preventDefault()
+        const token = localStorage.getItem("token")
+
+        console.log(newcomment);
+
+        await axios.request({
+            headers:{
+                Authorization: `Bearer ${token}`
+            },
+            method: "POST",
+            url: "http://localhost:8080/comment/add",
+            data: newcomment
+        }).then(response=>{
+            navigate(`/project/${id}`)
+        })
+    }
+
+    const handleInput = (e)=>{
+        setComment({...newcomment, [e.target.name]:e.target.value})
+    }
+
+  return (
+    <>
+        <form onSubmit={(e)=>createComment(e)}>
+            <input type="text" id='comment' name='comment' required value={comment} onChange={(e)=>handleInput(e)}/>
+            <button type='submit'>Yorum Yap</button>
+        </form>
+    </>
+  )
+}
+
+export default CreateComment
