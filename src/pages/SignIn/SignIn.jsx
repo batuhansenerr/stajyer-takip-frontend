f2fe05208cdd536a80d8f9c9dc6b29d4c90cb715
abatuhan06 <abatuhan0666@gmail.com>
2024-05-07 09:29:33 +0300

    sign in refactored

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import logo  from "../../assets/img/logo.png"
+import './SignIn.css'
 
 const SignIn = () => {
 
@@ -21,24 +23,29 @@ const login = async(e) => {
     e.preventDefault()
     const request = await axios.post("http://localhost:8080/auth/login", user)
     if(request.status===200){
-        console.log("girdik", request);
+        console.log("log in ->", request);
         localStorage.setItem("token", request.data)
-        navigate("/profile")
+        navigate("/projects")
     }
 }
 
   return (
     <>
-        <form onSubmit={(e)=>login(e)}>
-        <div>
-            <label htmlFor="username">Kullanıcı Adı </label>
-            <input type="text" id='username' name='username' required value={username} onChange={(e)=>handleInput(e)}/>
+        <form className='loginPageCard' onSubmit={(e)=>login(e)}>
+        <div className="logo">
+            <img src={logo} alt="" />
         </div>
-        <div>
-            <label htmlFor="password">Şifre </label>
-            <input type="password" id='password' name='password' required value={password} onChange={(e)=>handleInput(e)}/>
+        <div className="inputs">
+            <div>
+                <label htmlFor="username">Kullanıcı Adı: </label>
+                <input type="text" id='username' name='username' required value={username} onChange={(e)=>handleInput(e)}/>
+            </div>
+            <div>
+                <label htmlFor="password">Şifre: </label>
+                <input type="password" id='password' name='password' required value={password} onChange={(e)=>handleInput(e)}/>
+            </div>
+            <button type='submit'>Giriş Yap</button>
         </div>
-        <button type='submit'>Giriş Yap</button>
         </form>
     </>
   )
