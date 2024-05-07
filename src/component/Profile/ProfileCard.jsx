635a569b9ba801cc1288b0b0c1c377dc7d0eef32
abatuhan06 <abatuhan0666@gmail.com>
2024-05-07 09:35:27 +0300

    profileCard reafactored

--- src/component/Profile/ProfileCard.jsx
+++ src/component/Profile/ProfileCard.jsx
@@ -1,56 +1,100 @@
-import React from 'react'
-import './ProfileCard.css'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './ProfileCard.css';
+import { useNavigate } from 'react-router-dom';
 
-const ProfileCard = ({user}) => {
-
-
-    /*const [isEditing, setIsEditing] = useState(false);
-    const [editedComment, setEditedComment] = useState("");
+const ProfileCard = ({ user }) => {
+    const navigate = useNavigate()
+    const [isEditing, setIsEditing] = useState(false);
+    const [newUser, setNewUser] = useState({
+        username: user.username,
+        name: user.name,
+        surname: user.surname,
+        oldPassword: "",
+        password: ""
+    });
 
     const handleEditClick = () => {
         setIsEditing(true);
-        setEditedComment(newcomment.comment);
+        setNewUser({
+            username: user.username,
+            name: user.name,
+            surname: user.surname,
+            oldPassword: "",
+            password: ""
+        });
     };
 
     const handleCancelEdit = () => {
         setIsEditing(false);
-    };
+    }
+
+    const logOut = () => {
+        localStorage.clear()
+        navigate("/login")
+    }
 
     const handleSaveEdit = async () => {
         const token = localStorage.getItem("token");
-        const updatedComment = { ...newcomment, comment: editedComment };
 
         await axios.request({
             headers: {
                 Authorization: `Bearer ${token}`
             },
             method: "PATCH",
-            url: `http://localhost:8080/comment/update/${id}`,
-            data: updatedComment
+            url: `http://localhost:8080/auth/update`,
+            data: newUser
         }).then(response => {
-            setNewcomment(response.data);
-            setIsEditing(false);
+            setIsEditing(false)
         });
-    };*/
+    };
 
-  return (
-    <>
-        <div className="profileCard">
-            <img src="" alt="" className='profileCardImage'/>
-            <div className="profileCardDetails">
-                <h2>{user.name} {user.surname}</h2>
-                <h3>@{user.username}</h3>
-                {user.authorities.map((auth, index)=>(
-                    <h4>- {auth.split('_')[1]}</h4>
-                ))}
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewUser({ ...newUser, [name]: value });
+    };
+
+    return (
+        <>
+            <div className="profileCard">
+                <img src="" alt="" className='profileCardImage' />
+                <div className="profileCardDetails">
+                    {isEditing ? (
+                        <>
+                        <div className="inputs">
+                            <input className='inputItem' type="text" placeholder='İsim' name="name" value={newUser.name} onChange={handleChange} />
+                            <input className='inputItem' type="text" placeholder='Soyisim' name="surname" value={newUser.surname} onChange={handleChange} />
+                            <input className='inputItem' type="text" placeholder='username' name="username" value={newUser.username} onChange={handleChange} />
+                            <input className='inputItem' type="text" placeholder='Şifre (* Yeni Şifre İçin Gerekli)' name="oldPassword" value={newUser.oldPassword} onChange={handleChange} />
+                            <input className='inputItem' type="text" placeholder='Yeni Şifre' name="password" value={newUser.password} onChange={handleChange} />
+                        </div>
+                        </>
+                    ) : (
+                        <>
+                            <h2>{user.name} {user.surname}</h2>
+                            <h3>@{user.username}</h3>
+                            {user.authorities.map((auth, index) => (
+                                <h4 key={index}>- {auth.split('_')[1]}</h4>
+                            ))}
+                        </>
+                    )}
+                </div>
+                <div className="profileCardButtons">
+                    {isEditing ? (
+                        <>
+                            <button className="saveButton" onClick={handleSaveEdit}>Kaydet</button>
+                            <button className="cancelButton" onClick={handleCancelEdit}>İptal</button>
+                        </>
+                    ) : (
+                        <>
+                        <button className="editButton" onClick={handleEditClick}>Düzenle</button>
+                        <button className="logOutButton" onClick={logOut}>Çıkış Yap</button>
+                        </>
+                    )}
+                </div>
             </div>
-            <div className="profileCardButtons">
-                <button className="editButton">Düzenle</button>
-                <button className="logOutButton">Çıkış Yap</button>
-            </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
-export default ProfileCard
+export default ProfileCard;
